453493ce9ddfb6c763c9daa61ebcb229f931c568
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-08-18 16:10:34 -0700

    include react in the bundle

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -61,16 +61,6 @@ module.exports = {
       }
     })
   ],
-  externals: [
-    {
-      "react": {
-        root: "React",
-        commonjs2: "react",
-        commonjs: "react",
-        amd: "react"
-      }
-    }
-  ],
   module: {
     loaders: [
       {
